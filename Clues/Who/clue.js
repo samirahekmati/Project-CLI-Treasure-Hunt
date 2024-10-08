4c06fd9e2fadb3d7c7868aab761a79235ffd8f99
Daniel Wagner-Hall <dawagner@gmail.com>
2024-10-08 13:15:17 +0100

    Add submission instructions to the final step
    These commands _should_ be pre-installed on all OSes we support (and
    even Windows!), and give a hook for folks to investigate what they are
    if they're curious.

--- Clues/Who/clue.js
+++ Clues/Who/clue.js
@@ -1,3 +1,20 @@
+let shaCommand;
+switch (process.platform) {
+  case "darwin":
+    shaCommand = "shasum -a 256 answer.txt";
+    break;
+  case "linux":
+    shaCommand = "sha256sum answer.txt";
+    break;
+  case "win32":
+    shaCommand = "certutil -hashfile answer.txt SHA256"
+    break;
+  default:
+    console.log(`Oh no, we didn't recognise your operating system, process.platform = ${process.platform}`);
+    process.exit(1);
+    break;
+}
+
 console.log(`Who is never gonna give you up?
 Never gonna take you down?
 Never gonna run around and desert you?
@@ -6,4 +23,10 @@ Put your answer in the answer.txt file you created.
 echo "ANSWER HERE" > answer.txt
 Now move this file to the Answer folder.
 How will you achieve this? Stay in the command line. (You can Google.)
+
+When you're done, run:
+
+${shaCommand}
+
+And paste the output into Slack thread 🧵 (create a new thread if there isn't one already).
 `)
